e185a9734c0e1139778104f70ac961a24d4f1894
yhash01 <63485348+yhash01@users.noreply.github.com>
2022-03-02 17:18:14 +0900

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,10 +1,6 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket-2"
-  acl    = "private"
+module "s3-bucket" {
+  source  = "app.terraform.io/yhash01-training/s3-bucket/aws"
   version = "2.2.0"
-
-  
-
+  # insert required variables here
 }
+  
